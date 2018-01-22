2f9199e56a4ca993001dddaeefd31304654a940a
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-22 13:31:41 +0200

    Move all site files to public subfolder

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from bottle import route, run, static_file
 
+
 @route('/')
 def home():
-    return static_file('index.html', root='')
+    return static_file('index.html', root='public')
+
 
 @route('/<filename>')
 def server_static(filename):
-    return static_file(filename, root='')
+    return static_file(filename, root='public')
+
 
 run(host='localhost', port=8080, debug=True)
